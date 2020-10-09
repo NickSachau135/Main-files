18fe61f7e355c2b35aede2e460a55bf22d01d909
Nick Sachau <nicksachau156@gmail.com>
2020-10-09 08:36:58 -0700

    updates to math calculator

--- Math calculator/script.js
+++ Math calculator/script.js
@@ -1,16 +1,6 @@
-
-var li = document.getElementById('number').innerHTML;
-li.innerHTML = `<li>
-<label for="num">
-    Enter A Number:
-    <input id="num" name="number" type="text">
-</label>
-</li>
-<li>
-    <button onclick="newNumber()">Another Number?</button>
-</li>`
-
 function newNumber() {
-    
-    li.innerHTML += `<li> <label for="num"> Enter A Number: <input id="num" name="number" type="text"> </label> </li>`
+	document.getElementById("number").innerHTML += `<li>
+		<label for="num"> Enter a Number:
+    	<input id="num" type="text">
+    </li>`;
 }
