3eb9adc296c65bc0b0cd676e2980664b70ec677f
Nick Sachau <nicksachau156@gmail.com>
2020-11-03 13:03:35 -0700

    update unit 4

--- Unit 4/Practice/10-29-2020/script.js
+++ Unit 4/Practice/10-29-2020/script.js
@@ -42,64 +42,59 @@
 //     console.log(`You Are Safe From the Cum Zone`)
 // }
 
-// let points = 0;
+let points = 0;
 
-// let math = prompt (`What is 2 + 2`);
-// let earBuds = prompt (`What ear buds are better: Pixel Buds or Air Pods?`);
-// let phone = prompt(`Which phone is beter: Iphone 10 or Pixel 4XL?`);
-// let cpu = prompt(`What CPU is better: I9 9900k or I7 9700k?`);
-// let language = prompt(`What coding language is better: C++ or Python?`);
+let math = prompt (`What is 2 + 2`);
+let earBuds = prompt (`What ear buds are better: Pixel Buds or Air Pods?`);
+let phone = prompt(`Which phone is beter: Iphone 10 or Pixel 4XL?`);
+let cpu = prompt(`What CPU is better: I9 9900k or I7 9700k?`);
+let language = prompt(`What coding language is better: C++ or Python?`);
 
-let a = document.getElementById('question_1').value;
-let x = document.write('Test');
-let y = document.getElementById.innerHTML= x;
+if (math == 4) {
+    console.log(`Correct! 2 + 2 = 4`);
+    points++;
+    console.log(`You have ${points} points!`);
 
+    document.getElementById("question_1").innerHTML = 2 + 3;
+} else {
+    console.log(`Incorrect! 2 + 2 = 4 not ${math}`);
+    console.log(`You have ${points} points!`);
 
-// if (math == 4) {
-//     console.log(`Correct! 2 + 2 = 4`);
-//     points++;
-//     console.log(`You have ${points} points!`);
+    document.getElementById('question_1').innerHTML = 2 + 2;
+}
 
-//     document.getElementById("question_1").innerHTML = 2 + 3;
-// } else {
-//     console.log(`Incorrect! 2 + 2 = 4 not ${math}`);
-//     console.log(`You have ${points} points!`);
+if (earBuds == "Pixel Buds") {
+    console.log(`Correct! Pixel Buds are better than Air Pods`);
+    points++
+    console.log(`You have ${points} points!`);
+}else {
+    console.log(`Disgusting!! Air Pods are not better than Pixel Buds.`);
+    console.log(`You have ${points} points!`);
+}
 
-//     document.getElementById('question_1').innerHTML = 2 + 2;
-// }
+if (phone == "Pixel 4XL") {
+    console.log(`Correct! Apple is bad!`);
+    points++
+    console.log(`You have ${points} points!`);
+}else {
+    console.log(`Disgusting!! Apple is bad! Never use apple!`);
+    console.log(`You have ${points} points!`);
+}
 
-// if (earBuds == "Pixel Buds") {
-//     console.log(`Correct! Pixel Buds are better than Air Pods`);
-//     points++
-//     console.log(`You have ${points} points!`);
-// }else {
-//     console.log(`Disgusting!! Air Pods are not better than Pixel Buds.`);
-//     console.log(`You have ${points} points!`);
-// }
+if (cpu == "I9 9900k") {
+    console.log(`Correct!!`);
+    points ++
+    console.log(`You have ${points} points!`);
+}else {
+    console.log(`What are you poor?!??`);
+    console.log(`You have ${points} points!`);
+}
 
-// if (phone == "Pixel 4XL") {
-//     console.log(`Correct! Apple is bad!`);
-//     points++
-//     console.log(`You have ${points} points!`);
-// }else {
-//     console.log(`Disgusting!! Apple is bad! Never use apple!`);
-//     console.log(`You have ${points} points!`);
-// }
-
-// if (cpu == "I9 9900k") {
-//     console.log(`Correct!!`);
-//     points ++
-//     console.log(`You have ${points} points!`);
-// }else {
-//     console.log(`What are you poor?!??`);
-//     console.log(`You have ${points} points!`);
-// }
-
-// if (language == "C++") {
-//     console.log(`Correct!! Python is bad!`);
-//     points++
-//     console.log(`You have ${points} points!`);
-// }else {
-//     console.log(`Python is bad, Dont use Python`);
-//     console.log(`You have ${points} points!`);
-// }
+if (language == "C++") {
+    console.log(`Correct!! Python is bad!`);
+    points++
+    console.log(`You have ${points} points!`);
+}else {
+    console.log(`Python is bad, Dont use Python`);
+    console.log(`You have ${points} points!`);
+}
---
+++ Unit 4/Practice/10-30-2020/script.js
@@ -0,0 +1,176 @@
+// if(name = "Pam") {
+//     console.log(`cool`);
+// }else {
+//     console.log(`not so cool`);
+// }
+
+// let coolness = (name = "Pam") ? "cool" : "not so cool";
+// console.log(`${coolness}`);
+
+// let result = (a + b < 4)  ? "Below" : "Over"; 
+
+// switch (param) {
+//     case "walk":
+//         break;
+//     case "sleep":
+        
+//     case "dream":
+//         break;
+//     default:
+// }
+
+// switch (true) {
+//     case (myVal >= 10):
+//         multipler = 2;
+//         break;
+//     case (myVal >= 0):
+//         multipler = 1;
+//         break;
+//     default:
+//         multipler = -1;
+// }
+
+// switch (login) {
+//     case "Employee":
+//         message = "Hello";
+//         break;
+//     case "Director":
+//         message == "Greeting"
+//         break;
+//     case "":
+//         message == "No Login";
+//         break;
+//     default:
+//         "";
+// }
+
+// if(employee == true) {
+//     if(name == "Karry"){
+//         if(computer == "personal") {
+
+//         }else if (computer == "work"){
+
+//         }
+//     }
+// }
+
+// || not II or LL or ll or ii 
+
+// if (shirt == "blue" || shirt == "red" || shirt == "green" || shirt == "pink"){
+
+// }
+
+// weekdays == true / false;
+// time = 14;
+
+// if (time < 9 || time > 17 || weekdays == false) {
+//     console.log(`The Store is closed`);
+// }else {
+//     console.log(`The store is open`)
+// }
+
+
+// tells you if you need to fix your car
+// if you have a flat tire
+// if you check engine light is on
+// if you have driven 30,000+ plus miles
+
+// let flatTire = false;
+// let checkEngineLight = true;
+// let miles = 30000;
+
+// if (flatTire || checkEngineLight || miles > 30000) {
+//     console.log(`Fix you car`);
+// }else {
+//     console.log(`You're good`)
+// }
+
+// let pen = true;
+// let paper = false;
+// let mouse = false;
+// let keyboard = true;
+
+// if (pen && paper || mouse && keyboard) {
+//     console.log(`You can take notes`)
+// }
+
+// let upLate = true;
+// let gameTooMuch = true;
+// let breakfast = false;
+
+// if (!upLate && !gameTooMuch && !breakfast){
+//     console.log(`pass`)
+// }
+
+// console.log(null || 2 && 3 || 4);
+
+// for(let i = 0; i < 10; i++) {
+// 	console.log(`this has happened ${i +1} times`);
+// }
+
+// for(let i = 5; i < 14; i+= 2 ){
+//     console.log(`The number prints ${i}`);
+// }
+
+
+// for(let i = 5; i < 12; i++) {
+//     console.log(i);
+// }
+
+// for(let i = 0; i < 9; i++) {
+//     console.log(`This is nice`);
+// }
+
+// for(let i = 11; i >= 1; i-= 2) {
+//     console.log(i)
+// }
+
+// output = ''
+// for(let i = 0; i < 5; i++ ) {
+//     let x = Math.floor(Math.random() * 100);
+//     output = output + ', ' + x;
+// }
+// console.log(output);
+
+
+// for(let i = 0; i < 5; i++ ) {
+//     let x = Math.floor(Math.random() * 10);
+//     console.log(x);
+//     const count = x.push(', ');
+//     console.log(count);
+// }
+
+// let sum = 0;
+// let amount = 0;
+
+// while(true) {
+//     let value = +prompt(`Enter a test score:`, `leave black if done`);
+//     if (!value) break;
+//     sum += Number(value);
+//     amount++;
+// }
+
+// let average = sum / amount;
+// let grade = '';
+
+// if(average >= 90){
+//     Grade = 'A';
+// }else if(average >= 80){
+//     Grade = 'B';
+// }else if(average >= 70){
+//     Grade = 'C';
+// }else if(average >= 60){
+//     Grade = 'D';
+// }else{
+//     Grade = 'F';
+// }
+
+// console.log(`Sum: ${sum}`);
+// console.log(`Amount: ${amount}`);
+// console.log(`Average: ${average}`);
+// console.log(`Grade: ${grade}`);
+
+// for (let i = 0; i < 100; i++) {
+//     if(i % 2 == 1) continue;
+//     console.log(i); 
+// }
