f0444b66f756c5e87df70c3bf7523d5ea18a2d20
nick sachau <nicksachau156@gmail.com>
2021-02-16 13:17:52 -0700

    '

--- Section 2/Intro to SASS/script.js
+++ Section 2/Intro to SASS/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+testing.addEventListener('click', function() {
+    if(main.classList.contains("theme-1")) {
+        main.classList.remove("theme-1")
+        main.classList.add("theme-2")
+    }else {
+        main.classList.remove('theme-2')
+        main.classList.add('theme-1')
+    }
+})
